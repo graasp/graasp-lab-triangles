c4e6730b242527934c540696351fe1aded7d2cc0
Mashkour <azizmashkour@gmail.com>
2018-09-26 18:14:23 +0200

    Add Dynamic triangle sizes

--- src/components/preview/Tri.js
+++ src/components/preview/Tri.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { Layer, Shape } from "react-konva";
+import React, { Component } from 'react';
+import { Layer, Shape } from 'react-konva';
 
-const Tri = () => (
-  <Layer>
-    <Shape
-      sceneFunc={(context, shape) => {
-        context.beginPath();
-        context.moveTo(150, 45);
-        context.lineTo(50, 74);
-        context.lineTo(100, 200);
-        context.closePath();
-        // (!) Konva specific method, it is very important
-        context.fillStrokeShape(shape);
-      }}
-      fill="white"
-      stroke="blue"
-      strokeWidth={4}
-    />
-  </Layer>
-);
+class Tri extends Component {
+  render() {
+    return (
+      <Layer>
+        <Shape
+          sceneFunc={(context, shape) => {
+            context.beginPath();
+            context.moveTo(this.props.points[0].x, this.props.points[0].y);
+            context.lineTo(this.props.points[1].x, this.props.points[1].y);
+            context.lineTo(this.props.points[2].x, this.props.points[2].y);
+            context.closePath();
+            context.fillStrokeShape(shape);
+          }}
+          fill="white"
+          stroke={this.props.color}
+          strokeWidth={1}
+        />
+      </Layer>
+    );
+  }
+}
 
 export default Tri;
