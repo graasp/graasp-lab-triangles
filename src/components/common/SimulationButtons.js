e286c4d9f0a903ff4c4b74286272728f7f204b09
Mashkour <azizmashkour@gmail.com>
2018-09-27 18:15:16 +0200

    Trying to run simulation and reseting buttons

--- src/components/common/SimulationButtons.js
+++ src/components/common/SimulationButtons.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Row } from 'reactstrap';
+import PropTypes from 'prop-types';
+class SimulationButtons extends Component {
+  static propTypes = {
+    handleReset: PropTypes.func.isRequired,
+  }
 
-const SimulationButtons = () => (
-  <Row className="pt-5">
-    <div className="ml-auto">
-      <Button color="primary mr-3">Reset</Button>
-      <Button color="outline-secondary">Simulate</Button>
-    </div>
-  </Row>
-);
+  handleResetChange = () => {
+    console.log('this is the Reset button:', this);
+    const { handleReset } = this.props;
+    const coords = [{ x: 50000, y: 40 }, { x: 200, y: 100 }, { x: 100, y: 150 }];
+    handleReset(coords);
+  }
+
+  handleSimulate = () => {
+    console.log('this is is The Simulation button:', this);
+  }
+
+  render() {
+    return (
+      <Row className="pt-5">
+        <div className="ml-auto">
+          <Button color="primary mr-3" onClick={this.handleResetChange}>Reset</Button>
+          <Button color="outline-secondary" onClick={this.handleSimulate}>Simulate</Button>
+        </div>
+      </Row>
+    );
+  }
+}
 
 export default SimulationButtons;
