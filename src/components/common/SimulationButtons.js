227cae850c01f61e5a241cbee0ae77dcab0b02ba
Mashkour <azizmashkour@gmail.com>
2018-10-01 16:43:01 +0200

    Get each Triangle dot datas and compare and render corresponding message

--- src/components/common/SimulationButtons.js
+++ src/components/common/SimulationButtons.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { Button, Row } from 'reactstrap';
-class SimulationButtons extends Component {
+import { Alert, Button, Row } from 'reactstrap';
 
+class SimulationButtons extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      coords: this.props.coords
+      coords: this.props.coords,
+      visible: true,
+      success: {
+        val: '',
+        val1: '',
+        val2: '',
+      },
     };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss() {
+    this.setState({ visible: false });
   }
 
   handleResetChange = () => {
@@ -15,58 +26,64 @@ class SimulationButtons extends Component {
   }
 
   handleSimulate = () => {
-    const { sets: {triA, triB} } = this.props;
-    console.log(triA, triB);
+    const { sets: {triOne, triTwo} } = this.props;
 
     // Getting Triangle ABC coordinates
-    const xa = triA[0].x;
-    const xb = triA[1].x;
-    const xc = triA[2].x;
-    const ya = triA[0].y;
-    const yb = triA[1].y;
-    const yc = triA[2].y;
+    const xa = triOne[0].x;
+    const xb = triOne[1].x;
+    const xc = triOne[2].x;
+    const ya = triOne[0].y;
+    const yb = triOne[1].y;
+    const yc = triOne[2].y;
 
     // Calculating triangle ABC distances
     // Calculating xAB and yAB
-    const dAB = Math.trunc(Math.sqrt(xb - xa)) + Math.trunc(Math.sqrt(yb - ya));
-    console.log('dAb', dAB);
+    const dAB = Math.trunc(Math.sqrt((xb - xa) * (xb - xa)))
+    + Math.trunc(Math.sqrt((yb - ya) * (yb - ya)));
 
     // Calculating xAC and yAC
-    const dAC = Math.trunc(Math.sqrt(xc - xa)) + Math.trunc(Math.sqrt(yc - ya));
+    const dAC = Math.trunc(Math.sqrt((xc - xa) * (xc - xa)))
+    + Math.trunc(Math.sqrt((yc - ya) * (yc - ya)));
 
     // Calculating xBC and yBC
-    const dBC = Math.trunc(Math.sqrt(xc - xb)) + Math.trunc(Math.sqrt(yc - yb));
+    const dBC = Math.trunc(Math.sqrt((xc - xb) * (xc - xb)))
+    + Math.trunc(Math.sqrt((yc - yb) * (yc - yb)));
 
     // Getting Triangle A`B`C` coordinates
-    const xaprim = triB[0].x;
-    const xbprim = triB[1].x;
-    const xcprim = triB[2].x;
-    const yaprim = triB[0].y;
-    const ybprim = triB[1].y;
-    const ycprim = triB[2].y;
+    const xaprim = triTwo[0].x;
+    const xbprim = triTwo[1].x;
+    const xcprim = triTwo[2].x;
+    const yaprim = triTwo[0].y;
+    const ybprim = triTwo[1].y;
+    const ycprim = triTwo[2].y;
 
     // Calculating triangle A`B`C` distances
     // Calculating xA`B` and yA`B`
-    const dABprim = Math.trunc(Math.sqrt(xbprim - xaprim)) + Math.trunc(Math.sqrt(ybprim - yaprim));
-    console.log('dABprim', dABprim);
+    const dABprim = Math.trunc(Math.sqrt((xbprim - xaprim)
+    * (xbprim - xaprim))) + Math.trunc(Math.sqrt(((ybprim - yaprim) * (ybprim - yaprim))));
 
     // Calculating xAC and yAC
-    const dACprim = Math.trunc(Math.sqrt(xcprim - xaprim)) + Math.trunc(Math.sqrt(ycprim - yaprim));
+    const dACprim = Math.trunc(Math.sqrt((xcprim - xaprim)
+    * (xcprim - xaprim))) + Math.trunc(Math.sqrt((ycprim - yaprim) * (ycprim - yaprim)));
 
     // Calculating xBC and yBC
-    const dBCprim = Math.trunc(Math.sqrt(xcprim - xbprim)) + Math.trunc(Math.sqrt(ycprim - ybprim));
-
+    const dBCprim = Math.trunc(Math.sqrt((xcprim - xbprim)
+    * (xcprim - xbprim))) + Math.trunc(Math.sqrt((ycprim - ybprim) * (ycprim - ybprim)));
     // Calculation side compareason
     const val = dAB / dABprim;
     const val1 = dAC / dACprim;
     const val2 = dBC / dBCprim;
-    console.log('val', val, val1, val2);
-
+    this.setState({ success: { val, val1, val2 } });
   }
 
   render() {
+    const { success } = this.state;
+    const { val, val1, val2 } = success;
     return (
       <Row className="pt-5">
+        { ((val == val1) == val2) ?
+          <Alert color="success" isOpen={this.state.visible} toggle={this.onDismiss}>Youpiii <span>😅</span>! Tri ABC and DEF are seemless</Alert>
+          : <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>Ups Sorry <span>🌚</span>! Tri ABC and DEF are not seemless</Alert>}
         <div className="ml-auto">
           <Button color="primary mr-3" onClick={this.handleResetChange}>Reset</Button>
           <Button color="outline-secondary" onClick={this.handleSimulate}>Simulate</Button>
