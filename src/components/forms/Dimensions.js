7fa02354d375ca661ee58fe0ebefcab00c27a253
Mashkour <azizmashkour@gmail.com>
2018-10-09 15:30:20 +0200

    fix: Remove all simulation buttons functions from FormViews Class to Dimension Class

--- src/components/forms/Dimensions.js
+++ src/components/forms/Dimensions.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  CardTitle, Col, Row,
+  Button, CardTitle, Col, Row,
 } from 'reactstrap';
 import { Stage } from 'react-konva';
 import Tri from '../preview/Tri';
@@ -15,19 +15,62 @@ class Dimensions extends Component {
       B: PropTypes.string.isRequired,
       C: PropTypes.string.isRequired,
     }).isRequired,
+    clax: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
+    triangles: PropTypes.string.isRequired,
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      points: [
-        { x: 50, y: 50 },
-        { x: 200, y: 100 },
-        { x: 100, y: 150 },
-      ],
+      points: props.triangles,
     };
     this.handlePChange = this.handlePChange.bind(this);
+  }
+
+  handleShiftRight = () => {
+    const { points } = this.state;
+    const newPoints = [...points];
+
+    const reachedLimit = newPoints.some(point => point.x > 450);
+    if (!reachedLimit) {
+      newPoints.forEach((point) => {
+        // eslint-disable-next-line no-param-reassign
+        point.x += 20;
+      });
+      this.handlePChange(newPoints);
+    }
+  }
+
+  handleShiftLeft = () => {
+    const { points } = this.state;
+    const newPoints = [...points];
+    const reachedLimit = newPoints.some(point => point.x < 20);
+    if (!reachedLimit) {
+      // eslint-disable-next-line no-param-reassign
+      newPoints.forEach((point) => { point.x -= 20; });
+      this.handlePChange(newPoints);
+    }
+  }
+
+  handleRotate = (event) => {
+    const { points } = this.state;
+    const newPoints = [...points];
+    const Ox = (newPoints[0].x + newPoints[1].x + newPoints[2].x) / 3;
+    const Oy = (newPoints[0].y + newPoints[1].y + newPoints[2].y) / 3;
+    const radians = event.target.dataset.rotate === 'right' ? (Math.PI / 180) * 90 : (Math.PI / 180) * (-90);
+    const cos = Math.cos(radians);
+    const sin = Math.sin(radians);
+
+    newPoints.forEach((point) => {
+      const nx = (cos * (point.x - Ox)) + (sin * (point.y - Oy)) + Ox;
+      const ny = (cos * (point.y - Oy)) - (sin * (point.x - Ox)) + Oy;
+      // eslint-disable-next-line no-param-reassign
+      point.x = Math.round(nx);
+      // eslint-disable-next-line no-param-reassign
+      point.y = Math.round(ny);
+    });
+    this.handlePChange(newPoints);
   }
 
   handlePChange(coordinates) {
@@ -38,7 +81,7 @@ class Dimensions extends Component {
 
   render() {
     const { points } = this.state;
-    const { node, color } = this.props;
+    const { node, color, clax } = this.props;
     return (
       <Row>
         <Col md="6" className="right-border pt-4">
@@ -57,7 +100,7 @@ class Dimensions extends Component {
           />
         </Col>
 
-        <Stage width="300" height="300" className="border-top">
+        <Stage width="450" height="300" className="border-top">
           <Tri
             points={
               [
@@ -67,10 +110,18 @@ class Dimensions extends Component {
               ]
             }
             color={color}
+            node={node}
           />
         </Stage>
+        <div className={clax}>
+          <Button color="success" size="sm" onClick={this.handleShiftLeft}>Shift L</Button>
+          <Button color="outline-warning" size="sm" className="ml-3 mr-3" onClick={this.handleRotate} data-rotate="left">Rotate L</Button>
+          <Button color="outline-warning" size="sm" className="ml-3 mr-3" onClick={this.handleRotate} data-rotate="right">Rotate R</Button>
+          <Button color="success" size="sm" onClick={this.handleShiftRight}>Shift R</Button>
+        </div>
       </Row>
     );
   }
 }
+
 export default Dimensions;
