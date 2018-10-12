6689085527ce17ddb6e3971c925e96baea0c57d4
Mashkour <azizmashkour@gmail.com>
2018-10-12 16:20:29 +0200

    test: implement first version of studentView test

--- src/components/__tests__/StudentView.test.js
+++ src/components/__tests__/StudentView.test.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import StudentView from '../../student/StudentView';
-
-let wrapped;
-beforeEach(() => {
-  wrapped = mount(<StudentView />);
-});
-
-afterEach(() => {
-  wrapped.unmount();
-});
+import Dimensions from '../forms/Dimensions';
+import SimulationButtons from '../common/SimulationButtons';
 
 it('has one instance of student view', () => {
-  expect(wrapped.find('h1').length).toEqual(1);
+  const props = {
+    t: jest.fn(),
+  };
+  const wrapped = shallow(<StudentView {...props} />);
+  expect(wrapped.find('div.App').length).toEqual(1);
+});
+
+it('has one instance of simulation button', () => {
+  const props = {
+    t: jest.fn(),
+    flashed: true,
+    triangles: {
+      triOne: [],
+      triTwo: [],
+    },
+  };
+  const wrapped = shallow(<SimulationButtons {...props} />);
+  expect(wrapped.find(SimulationButtons).length).toEqual(0);
+});
+
+it('has two instances of dimension', () => {
+  const props = {
+    t: jest.fn(),
+    clax: 'class-One',
+    flashed: false,
+    color: 'rgb(29,103, 189)',
+    node: {
+      A: 'A',
+      B: 'B',
+      C: 'C',
+    },
+    triangles: [
+      { x: 20, y: 50 },
+      { x: 20, y: 50 },
+      { x: 20, y: 50 },
+    ],
+    updateDimensions: jest.fn(),
+  };
+  const wrapped = shallow(<Dimensions {...props} />);
+  expect(wrapped.find(Dimensions).length).toEqual(0);
 });
