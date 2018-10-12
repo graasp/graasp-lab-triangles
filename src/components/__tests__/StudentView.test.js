13bfd17b956162c536f563d29dfaae3bd0c7bd5f
Mashkour <azizmashkour@gmail.com>
2018-10-12 16:23:07 +0200

    test: use describe block for student view tests

--- src/components/__tests__/StudentView.test.js
+++ src/components/__tests__/StudentView.test.js
@@ -4,45 +4,47 @@ import StudentView from '../../student/StudentView';
 import Dimensions from '../forms/Dimensions';
 import SimulationButtons from '../common/SimulationButtons';
 
-it('has one instance of student view', () => {
-  const props = {
-    t: jest.fn(),
-  };
-  const wrapped = shallow(<StudentView {...props} />);
-  expect(wrapped.find('div.App').length).toEqual(1);
-});
+describe('render student view and run it componets tests', () => {
+  it('has one instance of student view', () => {
+    const props = {
+      t: jest.fn(),
+    };
+    const wrapped = shallow(<StudentView {...props} />);
+    expect(wrapped.find('div.App').length).toEqual(1);
+  });
 
-it('has one instance of simulation button', () => {
-  const props = {
-    t: jest.fn(),
-    flashed: true,
-    triangles: {
-      triOne: [],
-      triTwo: [],
-    },
-  };
-  const wrapped = shallow(<SimulationButtons {...props} />);
-  expect(wrapped.find(SimulationButtons).length).toEqual(0);
-});
+  it('has one instance of simulation button', () => {
+    const props = {
+      t: jest.fn(),
+      flashed: true,
+      triangles: {
+        triOne: [],
+        triTwo: [],
+      },
+    };
+    const wrapped = shallow(<SimulationButtons {...props} />);
+    expect(wrapped.find(SimulationButtons).length).toEqual(0);
+  });
 
-it('has two instances of dimension', () => {
-  const props = {
-    t: jest.fn(),
-    clax: 'class-One',
-    flashed: false,
-    color: 'rgb(29,103, 189)',
-    node: {
-      A: 'A',
-      B: 'B',
-      C: 'C',
-    },
-    triangles: [
-      { x: 20, y: 50 },
-      { x: 20, y: 50 },
-      { x: 20, y: 50 },
-    ],
-    updateDimensions: jest.fn(),
-  };
-  const wrapped = shallow(<Dimensions {...props} />);
-  expect(wrapped.find(Dimensions).length).toEqual(0);
+  it('has two instances of dimension', () => {
+    const props = {
+      t: jest.fn(),
+      clax: 'class-One',
+      flashed: false,
+      color: 'rgb(29,103, 189)',
+      node: {
+        A: 'A',
+        B: 'B',
+        C: 'C',
+      },
+      triangles: [
+        { x: 20, y: 50 },
+        { x: 20, y: 50 },
+        { x: 20, y: 50 },
+      ],
+      updateDimensions: jest.fn(),
+    };
+    const wrapped = shallow(<Dimensions {...props} />);
+    expect(wrapped.find(Dimensions).length).toEqual(0);
+  });
 });
