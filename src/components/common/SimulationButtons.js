c2227134441d9e1a17f27300770c52699925a6c5
Mashkour <azizmashkour@gmail.com>
2018-10-16 16:28:53 +0200

    fix: improve styles

--- src/components/common/SimulationButtons.js
+++ src/components/common/SimulationButtons.js
@@ -133,7 +133,7 @@ class SimulationButtons extends Component {
           </div>
         </Row>
         <div className="ml-3 mt-3">
-          <Button color="info p-3 mr-5 compare-btn" onClick={this.handleSimulate}>{t('compare')}</Button>
+          <Button className="primary-blued p-3 mr-5 compare-btn" onClick={this.handleSimulate}>{t('compare')}</Button>
         </div>
       </div>
     );
