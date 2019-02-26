1b7115b29986a8b2b47b20f9eb1d20227878b3f5
azizmashkour <azizmashkour@gmail.com>
2019-02-26 17:13:58 +0100

    feat: add grid dynamically and remove unused konva tags

--- src/components/preview/Tri.js
+++ src/components/preview/Tri.js
@@ -1,55 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Circle, Layer, Line, Shape, Text,
+  Circle, Shape, Text,
 } from 'react-konva';
 
 const Tri = ({
   color,
   node,
+  stroke,
   points,
   handleDragMove,
   handleDragMoveOne,
   handleDragMoveTwo,
   checkBoundaries,
+  handleMouseEnter,
+  handleMouseLeave,
+  strokeWidthOne,
+  strokeWidthTwo,
+  strokeWidthThree,
+  radiusOne,
 }) => (
-  <Layer>
-    <Line points={[0, 25, 1000, 25]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 50, 1000, 50]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 75, 1000, 75]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 100, 1000, 100]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 125, 1000, 125]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 150, 1000, 150]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 175, 1000, 175]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 200, 1000, 200]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 225, 1000, 225]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 250, 1000, 250]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 275, 1000, 275]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 300, 1000, 300]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 325, 1000, 325]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 350, 1000, 350]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 375, 1000, 375]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 400, 1000, 400]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[0, 425, 1000, 425]} stroke="#ced4da" strokeWidth={0.5} />
-
-    <Line points={[0, 0, 0, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[25, 0, 25, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[50, 0, 50, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[75, 0, 75, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[100, 0, 100, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[125, 0, 125, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[150, 0, 150, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[175, 0, 175, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[200, 0, 200, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[225, 0, 225, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[250, 0, 250, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[275, 0, 275, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[300, 0, 300, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[325, 0, 325, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[350, 0, 350, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[375, 0, 375, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[400, 0, 400, 1000]} stroke="#ced4da" strokeWidth={0.5} />
-    <Line points={[425, 0, 425, 1000]} stroke="#ced4da" strokeWidth={0.5} />
+  <Fragment>
     <Shape
       sceneFunc={(context, shape) => {
         context.beginPath();
@@ -60,21 +31,23 @@ const Tri = ({
         context.fillStrokeShape(shape);
       }}
       fill="rgb(232, 240, 249)"
-      stroke={color}
+      stroke={stroke}
       strokeWidth={3}
       opacity={0.5}
     />
     <Circle
       x={points[0].x}
       y={points[0].y}
-      radius={5}
+      radius={radiusOne}
       fill={color}
       stroke="#555"
-      strokeWidth={0.5}
+      strokeWidth={strokeWidthOne}
       shadowBlur={5}
       onDragMove={handleDragMove}
       draggable
       dragBoundFunc={pos => checkBoundaries(pos)}
+      onMouseEnter={e => handleMouseEnter(e, 'circleOne')}
+      onMouseLeave={e => handleMouseLeave(e, 'circleOne')}
     />
     <Circle
       x={points[1].x}
@@ -82,11 +55,13 @@ const Tri = ({
       radius={5}
       fill={color}
       stroke="#555"
-      strokeWidth={0.5}
+      strokeWidth={strokeWidthTwo}
       shadowBlur={5}
       onDragMove={handleDragMoveOne}
       draggable
       dragBoundFunc={pos => checkBoundaries(pos)}
+      onMouseEnter={e => handleMouseEnter(e, 'circleTwo')}
+      onMouseLeave={e => handleMouseLeave(e, 'circleTwo')}
     />
     <Circle
       x={points[2].x}
@@ -94,11 +69,13 @@ const Tri = ({
       radius={5}
       fill={color}
       stroke="#555"
-      strokeWidth={0.5}
+      strokeWidth={strokeWidthThree}
       shadowBlur={5}
       onDragMove={handleDragMoveTwo}
       draggable
       dragBoundFunc={pos => checkBoundaries(pos)}
+      onMouseEnter={e => handleMouseEnter(e, 'circleThree')}
+      onMouseLeave={e => handleMouseLeave(e, 'circleThree')}
     />
     <Text
       x={points[0].x + 2}
@@ -121,15 +98,22 @@ const Tri = ({
       fontSize={20}
       fill={color}
     />
-  </Layer>
+  </Fragment>
 );
 
 Tri.propTypes = {
   color: PropTypes.string.isRequired,
+  stroke: PropTypes.string.isRequired,
+  radiusOne: PropTypes.number.isRequired,
+  strokeWidthOne: PropTypes.number.isRequired,
+  strokeWidthTwo: PropTypes.number.isRequired,
+  strokeWidthThree: PropTypes.number.isRequired,
   handleDragMove: PropTypes.func.isRequired,
   checkBoundaries: PropTypes.func.isRequired,
   handleDragMoveOne: PropTypes.func.isRequired,
   handleDragMoveTwo: PropTypes.func.isRequired,
+  handleMouseEnter: PropTypes.func.isRequired,
+  handleMouseLeave: PropTypes.func.isRequired,
   node: PropTypes.shape({
     A: PropTypes.string.isRequired,
     B: PropTypes.string.isRequired,
