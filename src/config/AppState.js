53b96bdb5ae230ec6ae21c895f5f303c0fc4418e
azizmashkour <azizmashkour@gmail.com>
2019-02-26 17:14:43 +0100

    feat: create and setup all app configs

---
+++ src/config/AppState.js
@@ -0,0 +1,35 @@
+export const AppState = {
+  showTitle: true,
+  isMouseInsideCircleOne: false,
+  isMouseInsideCircleTwo: false,
+  isMouseInsideCircleThree: false,
+  openModal: false,
+  blockSnapSize: 50,
+  classOne: 'class-One',
+  classTwo: 'class-Two',
+  flashed: false,
+  colorOne: 'rgb(29,103, 189)',
+  colorTwo: 'rgb(29,103, 189)',
+  triOne: [
+    { x: 150, y: 50 },
+    { x: 450, y: 250 },
+    { x: 100, y: 450 },
+  ],
+  triTwo: [
+    { x: 750, y: 150 },
+    { x: 1100, y: 100 },
+    { x: 1000, y: 450 },
+  ],
+  nodeOne: {
+    A: 'A',
+    B: 'B',
+    C: 'C',
+  },
+  nodeTwo: {
+    A: 'D',
+    B: 'E',
+    C: 'F',
+  },
+};
+
+export default AppState;
